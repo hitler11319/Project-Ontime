eff49ff0e4eaacb2d8251aaf40a33a4d29933614
hitler11319 <hitler11319@gmail.com>
2016-03-08 20:06:24 +0800

    make clock

--- ontime.py
+++ ontime.py
@@ -1,24 +1,76 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QPoint, Qt, QTime, QTimer
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 class Clock(QWidget):
-    def __init__(self):
-        super().__init__()
+    hourHand = QPolygon([
+        QPoint(7, 8),
+        QPoint(-7, 8),
+        QPoint(0, -40)
+    ])
+
+    minuteHand = QPolygon([
+        QPoint(7, 8),
+        QPoint(-7, 8),
+        QPoint(0, -70)
+    ])
+
+    hourColor = QColor(127, 127, 127)
+    minuteColor = QColor(0, 152, 127, 191)
+
+    def __init__(self, parent=None):
+        super(Clock, self).__init__(parent)
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.update)
+        timer.start(1000)
+
+
+    def paintEvent(self, event):
+        side = min(self.width(), self.height())
+        time = QTime.currentTime()
+
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.translate(self.width() / 2, self.height() / 2)
+        painter.scale(side / 200.0, side / 200.0)
+
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(Clock.hourColor)
+
+        painter.save()
+        painter.rotate(30.0 * ((time.hour() + time.minute() / 60.0)))
+        painter.drawConvexPolygon(Clock.hourHand)
+        painter.restore()
+
+        painter.setPen(Clock.hourColor)
+
+        for i in range(12):
+            painter.drawLine(88, 0, 96, 0)
+            painter.rotate(30.0)
+
+        painter.setPen(Qt.NoPen)
+        painter.setBrush(Clock.minuteColor)
+
+        painter.save()
+        painter.rotate(6.0 * (time.minute() + time.second() / 60.0))
+        painter.drawConvexPolygon(Clock.minuteHand)
+        painter.restore()
+
+        painter.setPen(Clock.minuteColor)
+
+        for j in range(60):
+            if (j % 5) != 0:
+                painter.drawLine(92, 0, 96, 0)
+            painter.rotate(6.0)
         
-
-
-
-
-
-
 
 class Ontime(QMainWindow):
     def __init__(self):
-        super(Chat, self).__init__()
+        super(Ontime, self).__init__()
         self.setWindowTitle('ontime')
 
-        self.clockArea = ClockArea()
+        self.clockArea = Clock()
         self.setCentralWidget(self.clockArea)
 
         self.createActions()
@@ -54,5 +106,6 @@ if __name__ == '__main__':
 
     app = QApplication(sys.argv)
     clock = Ontime()
-    clcok.show()
+    clock.show()
     sys.exit(app.exec_())
+
